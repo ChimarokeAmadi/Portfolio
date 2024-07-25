e67d905d6028441d5c9e7d72f3757258ecadfb79
Chimaroke Amadi <chirokeamadi@gmail.com>
2024-07-25 09:56:01 +0100

    first portfolio commit

---
+++ src/constants/index.js
@@ -0,0 +1,63 @@
+import project1 from "../assets/projects/project1.png";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate frontend developer with a knack for crafting robust and scalable web applications. With over a year of hands-on experience, I have honed my skills in front-end technologies like React and Next.js. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile frontend developer with a passion for creating efficient and user-friendly  applications. With a year of experience, I have worked with a variety of technologies, including React, Next.js, React native etc. My journey in frontend development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    date: "March, 2024 - May, 2024",
+    role: "Intern",
+    company: "Oasis Infobyte",
+    description: `Implemented CSS and JavaScript technologies such as React and Bootstrap to create delightful user experience on various web and mobile apps`,
+    technologies: ["Javascript", "React.js", "React-Native"],
+  },
+  {
+    date: "February,2023 - February, 2024",
+    role: "Intern",
+    company: "MyAza",
+    description: `Implemented frontend development frameworks like react as well as frontend development principles like responsive design to provide enticing and user-friendly user interfaces and helped with code documentation, unit testing and APIs`,
+    technologies: ["HTML", "CSS", "ReactJs", "React Native"],
+  },
+
+];
+
+export const PROJECTS = [
+  {
+    title: "Real Estate Website",
+    image: project1,
+    description:
+      "A fully functional Real estate website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "Tailwind", "React"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Calculator App",
+    image: project4,
+    description:
+      "An exciting Calculator web app that allows users to solve mathematical problems both on mobile and desktop devices",
+    technologies: ["HTML", "Tailwind", "React"],
+  },
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+234 816 550 9263",
+  email: "chirokeamadi@gmail.com",
+};
